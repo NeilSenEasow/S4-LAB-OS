1ad6d261213d665f713ae6e860ced6871268ca7b
Neil Sen Easow <neilseneasow@gmail.com>
2024-07-08 12:14:02 +0530

    modifying banker's algo

--- Banker's algorithm/bankers.c
+++ Banker's algorithm/bankers.c
@@ -1,90 +1,90 @@
 #include <stdio.h>
 
 int main() {
-    int n, m, available[10], need[10][10], allocation[10][10], max[10][10];
-    int i, j, k, p[10], work[10], finish[10], index = 0, flag, y;
+    int n, m, i, j, k;
 
-    printf("Enter number of Resource Types: ");
-    scanf("%d", &m);
-    printf("Enter number of Processes: ");
+    // Get number of processes and resources from the user
+    printf("Enter the number of processes: ");
     scanf("%d", &n);
+    printf("Enter the number of resources: ");
+    scanf("%d", &m);
 
-    printf("Enter number of instances of each Resource Type:\n");
-    for (i = 0; i < m; i++) {
-        printf("Resource Type %d: ", i + 1);
-        scanf("%d", &available[i]);
-        work[i] = available[i];
-    }
+    int alloc[n][m], max[n][m], avail[m];
 
-    printf("Enter number of Allocated instances for each Process:\n");
+    // Get allocation matrix from the user
+    printf("Enter the allocation matrix:\n");
     for (i = 0; i < n; i++) {
-        printf("Process %d:\n", i + 1);
+        printf("Process %d: ", i + 1);
         for (j = 0; j < m; j++) {
-            printf("Resource Type %d: ", j + 1);
-            scanf("%d", &allocation[i][j]);
+            scanf("%d", &alloc[i][j]);
         }
     }
 
-    printf("Enter number of Maximum instances for each Process:\n");
+    // Get maximum matrix from the user
+    printf("Enter the maximum matrix:\n");
     for (i = 0; i < n; i++) {
-        printf("Process %d:\n", i + 1);
+        printf("Process %d: ", i + 1);
         for (j = 0; j < m; j++) {
-            printf("Resource Type %d: ", j + 1);
             scanf("%d", &max[i][j]);
         }
     }
 
-    // Initialize finish array
-    for (i = 0; i < n; i++) {
-        finish[i] = 0;
+    // Get available resources from the user
+    printf("Enter the available resources: ");
+    for (i = 0; i < m; i++) {
+        scanf("%d", &avail[i]);
     }
 
-    // Calculate need matrix
+    int f[n], ans[n], ind = 0;
+    for (k = 0; k < n; k++) {
+        f[k] = 0;
+    }
+
+    int need[n][m];
     for (i = 0; i < n; i++) {
         for (j = 0; j < m; j++) {
-            need[i][j] = max[i][j] - allocation[i][j];
+            need[i][j] = max[i][j] - alloc[i][j];
         }
     }
 
-    // Find the safe sequence
-    for (i = 0; i < n; i++) {
-        for (j = 0; j < n; j++) {
-            if (finish[j] == 0) {
-                flag = 0;
-                for (k = 0; k < m; k++) {
-                    if (need[j][k] > work[k]) {
+    int y = 0;
+    for (k = 0; k < n; k++) {
+        for (i = 0; i < n; i++) {
+            if (f[i] == 0) {
+                int flag = 0;
+                for (j = 0; j < m; j++) {
+                    if (need[i][j] > avail[j]) {
                         flag = 1;
                         break;
                     }
                 }
 
                 if (flag == 0) {
-                    p[index++] = j;
+                    ans[ind++] = i;
                     for (y = 0; y < m; y++) {
-                        work[y] += allocation[j][y];
+                        avail[y] += alloc[i][y];
                     }
-                    finish[j] = 1;
+                    f[i] = 1;
                 }
             }
         }
     }
 
-    // Check if all processes are finished
-    flag = 1;
+    int flag = 1;
     for (i = 0; i < n; i++) {
-        if (finish[i] == 0) {
+        if (f[i] == 0) {
             flag = 0;
-            printf("\nThe system is not in a safe state.\n");
+            printf("The following system is not safe\n");
             break;
         }
     }
 
     if (flag == 1) {
-        printf("\nSafe Sequence: ");
+        printf("Following is the SAFE Sequence\n");
         for (i = 0; i < n - 1; i++) {
-            printf("P%d -> ", p[i] + 1);
+            printf(" P%d ->", ans[i]);
         }
-        printf("P%d\n", p[n - 1] + 1);
+        printf(" P%d", ans[n - 1]);
     }
 
     return 0;
